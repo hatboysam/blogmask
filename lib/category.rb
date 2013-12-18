1f75451ae785cd3b35e592ac663763e1a7645c4f
hatboysam <hatboysam@gmail.com>
2013-12-18 01:43:18 -0500

    No slashes in Categories

--- lib/category.rb
+++ lib/category.rb
@@ -2,9 +2,9 @@ module Category
 
   # All of the available categories
   @@all = [ 'Other', 'Arts', 'Business', 'Confession', 'Entertainment', 'Funny', 'Fashion', 
-      'Gaming', 'Health', 'How To', 'Journal', 'Life', 'Men', 'Movies/TV', 
+      'Gaming', 'Health', 'How To', 'Journal', 'Life', 'Men', 'Movies', 
       'Music', 'Opinion', 'Politics', 'Science', 'Self', 'Society', 'Rant', 
-      'Religion', 'Sports', 'Technoloy', 'Travel', 'Women', 'World' ]
+      'Religion', 'Sports', 'Technoloy', 'Travel', 'TV' 'Women', 'World' ]
 
   def self.all
     @@all
